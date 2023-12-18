69388ceda2271b183c009ab61395e971b4360412
nirajsaraf9 <nirajsaraf9@gmail.com>
2023-12-18 15:41:36 +0530

    this is for confirm

---
+++ Day4Assignment/src/day4practiseproblem/EmployeeWageComputation.java
@@ -0,0 +1,12 @@
+package day4practiseproblem;
+
+//EmployeeWageComputation.java
+public class EmployeeWageComputation {
+
+
+ public static void main(String[] args) {
+     // Display welcome message
+     System.out.println("Welcome to Employee Wage Computation Program!");
+
+ }
+}
